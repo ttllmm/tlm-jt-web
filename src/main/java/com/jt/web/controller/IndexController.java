6ade48bac085e9ca8fc141d941aa006050db2c8f
tlm <568114272@qq.com>
2019-01-17 08:34:53 +0800

    2019.1.17 8:34

---
+++ src/main/java/com/jt/web/controller/IndexController.java
@@ -0,0 +1,10 @@
+package com.jt.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class IndexController {
+	
+
+}
